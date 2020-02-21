2d21869a7fb2571bdb6ea79c9ce5180d97668f07
dikinakub <dikinakub6509@gmail.com>
2020-02-21 14:01:08 +0700

    update frontend 21/02/20

--- PopProject/src/app/app-routing.module.ts
+++ PopProject/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CreateEditComponent } from './create-edit/create-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'create-edit', component: CreateEditComponent },
 ];
 
 @NgModule({
--- PopProject/src/app/app.module.ts
+++ PopProject/src/app/app.module.ts
@@ -4,25 +4,98 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import {MatCardModule} from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {  MatAutocompleteModule,
+          MatBadgeModule,
+          MatBottomSheetModule,
+          MatButtonModule,
+          MatButtonToggleModule,
+          MatCardModule,
+          MatCheckboxModule,
+          MatChipsModule,
+          MatDatepickerModule,
+          MatDialogModule,
+          MatDividerModule,
+          MatExpansionModule,
+          MatGridListModule,
+          MatIconModule,
+          MatInputModule,
+          MatListModule,
+          MatMenuModule,
+          MatNativeDateModule,
+          MatPaginatorModule,
+          MatProgressBarModule,
+          MatProgressSpinnerModule,
+          MatRadioModule,
+          MatRippleModule,
+          MatSelectModule,
+          MatSidenavModule,
+          MatSliderModule,
+          MatSlideToggleModule,
+          MatSnackBarModule,
+          MatSortModule,
+          MatStepperModule,
+          MatTableModule,
+          MatTabsModule,
+          MatToolbarModule,
+          MatTooltipModule,
+          MatTreeModule,
+} from '@angular/material';
+import { CreateEditComponent } from './create-edit/create-edit.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CreateEditComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatCardModule,
     BrowserAnimationsModule,
     MatSliderModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTooltipModule,
+    MatTreeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
