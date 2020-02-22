5ff5ba8c5b70a317450e4b4a079d03353cbd2d97
PtSitthiwong <putthamawadee.s@ils.co.th>
2020-02-22 16:28:27 +0700

    new project name and add spring boot

---
+++ PopProject-Java/src/main/java/com/ils/popProject/PopProject/PopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ils.popProject.PopProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopProjectApplication.class, args);
+	}
+
+}
---
+++ PopProject-Java/src/test/java/com/ils/popProject/PopProject/PopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ils.popProject.PopProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
